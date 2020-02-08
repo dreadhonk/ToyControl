23ad420de41feb83fba7ae6b3f0e63afe8c1b181
dreadhonk <dreadhonk@protonmail.com>
2020-02-08 00:00:04 +0000

    add MagnitudeNode

---
+++ app/src/main/java/eu/dreadhonk/apps/toycontrol/control/MagnitudeNode.kt
@@ -0,0 +1,15 @@
+package eu.dreadhonk.apps.toycontrol.control
+
+class MagnitudeNode(
+    nIO: Int = 3
+): Node {
+    override val inputs = FloatArray(nIO)
+    override val outputs = FloatArray(1)
+    override var invalidated: Boolean = true
+
+    override fun update(): Long {
+        outputs[0] = MathUtil.length(inputs)
+        invalidated = false
+        return ToyController.UPDATE_ON_INPUT_CHANGE
+    }
+}
