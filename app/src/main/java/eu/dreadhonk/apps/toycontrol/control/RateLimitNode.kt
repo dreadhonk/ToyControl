4cc536ab1c94e02bbfed67e4c1231a6c1cc6792b
dreadhonk <dreadhonk@protonmail.com>
2020-02-08 00:00:01 +0000

    extend ratelimitnode with support for gate function and constant rate
    the idea is to be able to emit values at a (nearly) constant rate.
    this would be useful for some types of filters which rely on data
    arriving at a given rate (for example, simple moving-average
    low pass filters).
    
    in addition, we introduce a gate function which allows to define
    which value will be passed downstream. this allows to take for
    example the max() of all values which were passed into the
    limiter while the limit was still in effect.
    
    this will be useful to re-create the shake effect which we
    currently hardcoded in the ControlView

--- app/src/main/java/eu/dreadhonk/apps/toycontrol/control/RateLimitNode.kt
+++ app/src/main/java/eu/dreadhonk/apps/toycontrol/control/RateLimitNode.kt
@@ -2,17 +2,28 @@ package eu.dreadhonk.apps.toycontrol.control
 
 import android.os.SystemClock
 
-class RateLimitNode(minPeriod: Long = 250, nIO: Int = 1): Node {
+class RateLimitNode(
+    public var minPeriod: Long = 250,
+    nIO: Int = 1,
+    public var gateFunction: (Float, Float) -> Float = {_, new -> new},
+    init: Float = 0.0f,
+    public var constantRate: Boolean = false): Node
+{
     override var invalidated: Boolean = true
     override val inputs = FloatArray(nIO)
+    private val buffer = FloatArray(nIO) { init }
     override val outputs = FloatArray(nIO)
 
     private var m_lastUpdate: Long = 0
-    public var minPeriod: Long = minPeriod
 
     override fun update(): Long {
         val timestamp = SystemClock.elapsedRealtime()
         val tooEarlyBy = m_lastUpdate + minPeriod - timestamp
+
+        for (i in inputs.indices) {
+            buffer[i] = gateFunction(buffer[i], inputs[i])
+        }
+
         if (tooEarlyBy > 0) {
             // ensure that this check re-runs on the next iteration
             invalidated = true
@@ -20,8 +31,15 @@ class RateLimitNode(minPeriod: Long = 250, nIO: Int = 1): Node {
         }
 
         m_lastUpdate = timestamp
-        inputs.copyInto(outputs)
-        invalidated = false
-        return ToyController.UPDATE_IMMEDIATELY
+        buffer.copyInto(outputs)
+        inputs.copyInto(buffer)
+
+        if (constantRate) {
+            invalidated = true
+            return minPeriod
+        } else {
+            invalidated = false
+            return ToyController.UPDATE_IMMEDIATELY
+        }
     }
 }
