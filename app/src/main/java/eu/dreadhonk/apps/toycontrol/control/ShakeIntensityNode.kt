e49a773275870ea9ce5fdd998a098691c5e8df8b
dreadhonk <dreadhonk@protonmail.com>
2020-02-11 00:00:02 +0000

    provisional ShakeIntensityNode
    modelled after the hack we currently have in the UI preview thing
    for control mode.

---
+++ app/src/main/java/eu/dreadhonk/apps/toycontrol/control/ShakeIntensityNode.kt
@@ -0,0 +1,32 @@
+package eu.dreadhonk.apps.toycontrol.control
+
+class ShakeIntensityNode: Node {
+    override var invalidated: Boolean = false
+
+    private val magnitude = MagnitudeNode(3)
+    private val rate = RateLimitNode(
+        minPeriod=50,
+        init=0.0f,
+        // we want to take the maximum magnitude for shake purposes
+        gateFunction={ old, new -> Math.max(old, new) },
+        nIO=3,
+        constantRate=true
+    )
+
+    override val inputs = magnitude.inputs
+    override val outputs = rate.outputs
+
+    override fun update(): Long {
+        // TODO: this needs scaling; when used with the TYPE_LINEAR_ACCELERATION sensor, this will
+        // generally output values between 0 and 25 or so, which is out of range for the normal
+        // flow.
+        inputs.copyInto(magnitude.inputs)
+        val result = magnitude.update()
+        if (result == ToyController.UPDATE_ON_INPUT_CHANGE) {
+            return result
+        }
+
+        magnitude.outputs.copyInto(rate.inputs)
+        return rate.update()
+    }
+}
